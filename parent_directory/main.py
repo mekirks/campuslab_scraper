e4a95d71fd4deeef83d3f2aa8cc9db0c151f29c5
Tyler <93991259+mekirks@users.noreply.github.com>
2021-11-23 10:33:05 -0500

    Update main.py
    ttu

--- parent_directory/main.py
+++ parent_directory/main.py
@@ -8,9 +8,9 @@ fun.scrape_multiples()
 
 #%%
 
-url = r'https://tigerlink.fhsu.edu/organizations'
+url = r'https://ttu.campuslabs.com/engage/organizations'
 uniID = r'10175'
-uni_name = r'Fort Hays State University'
+uni_name = r'Texas Tech University'
 
 fun.scrape_single(url, uniID, uni_name)
 # %%
